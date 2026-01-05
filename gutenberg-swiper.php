8818ce9dfdf816fc6ebf38f5e61968889b899c42
kacperramenda <kacper.ramenda1@gmail.com>
2026-01-05 20:38:39 +0100

    feat: register blocks and create block.json

--- gutenberg-swiper.php
+++ gutenberg-swiper.php
@@ -1,1 +1,20 @@
 <?php
+/**
+ * Plugin Name: Gutenberg Swiper
+ * Description: Slider oparty o gutenberg i Swiper.js
+ * Version: 1.0.0
+ * Author: Kacper Ramenda
+ * Text Domain: gutenberg-swiper
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function gutenberg_swiper_block_init(): void
+{
+    register_block_type(__DIR__ . '/src/container');
+    register_block_type(__DIR__ . '/src/slide');
+}
+
+add_action('init', 'gutenberg_swiper_block_init');
